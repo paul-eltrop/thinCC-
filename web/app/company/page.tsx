1e897d8020e72945f0a48fcf3b3eed7ad397f80a
Paul Eltrop <bfpeltrop@gmail.com>
2026-04-09 06:50:31 +0200

    fix

--- web/app/company/page.tsx
+++ web/app/company/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { CompanyDocuments } from '@/components/CompanyDocuments';
